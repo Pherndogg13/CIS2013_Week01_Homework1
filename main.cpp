1ffc6e255a8639bffef686e2da56869ab1e3c9f3
Pherndogg13 <46982716+Pherndogg13@users.noreply.github.com>
2019-01-30 21:26:23 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main( )
+{
+	cout << "=====================================================================\n";
+	cout << " Fernando Rosales\n";
+	cout << " Programmer, Oklahoma State University - Oklahoma City\n";
+	cout << " 12008 Garden Drive\n";
+	cout << " Oklahoma City, Ok\n";
+	cout << " p: (405) 213-9093\n";
+	cout << " e: Phern1988@gmail.com\n";
+	cout << " g: github.com/Pherndogg\n";
+	cout << " t: @13Ferndogg13\n";
+	cout << "=====================================================================\n";
+	return 0;
+}
